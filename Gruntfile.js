55ea095c696bfd6a97da5f0f74e58246665d03e2
Ahmad Nassri <ahmad@codeinchaos.com>
2014-02-16 19:35:11 -0500

    re-enabling qunit

--- Gruntfile.js
+++ Gruntfile.js
@@ -313,7 +313,7 @@ module.exports = function (grunt) {
         'jsonlint',
         'lesslint',
         'lint5',
-        //'qunit'
+        'qunit'
     ]);
 
     grunt.registerTask('default', [
